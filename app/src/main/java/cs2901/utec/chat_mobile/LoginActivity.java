e63a7001b675acc539ecb46a0661c0cdd6fa1ac3
alonsobs1311 <alonso.barrios@utec.edu.pe>
2019-07-03 09:55:54 -0500

    Show Users

--- app/src/main/java/cs2901/utec/chat_mobile/LoginActivity.java
+++ app/src/main/java/cs2901/utec/chat_mobile/LoginActivity.java
@@ -67,6 +67,8 @@ public class LoginActivity extends AppCompatActivity {
                         if(message.equals("Authorized")) {
                             showMessage("Authenticated");
                             Intent intent = new Intent(getActivity(), ContactsActivity.class);
+                            intent.putExtra("user_id", response.getInt("user_id"));
+                            intent.putExtra("username", response.getString("username"));
                             startActivity(intent);
                         }
                         else {
